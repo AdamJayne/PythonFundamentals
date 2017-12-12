17271f4597e222fb00a20633f1e2ae483123692c
Adam Jayne <adamjayne402@gmail.com>
2017-12-12 12:16:58 -0500

    added numbers to new docs

--- operators.py
+++ operators.py
@@ -2,6 +2,6 @@
 4 - 5  # Subtraction
 4 * 5  # Multiplication
 4 / 4  # Division
-13 // 4  # Rounds down the result to the nearest Whole Number
+13 // 4  # Rounds down the result to the nearest Whole Number  FLOOR DIVISION
 13 % 4 # Modulus (remainder)
 5 ** 3  # Exponent (5^3)
---
+++ pythonDocs/numbers.py
@@ -0,0 +1,9 @@
+# This is a comment
+# Numbers
+2 + 2
+50 - 5 * 6
+(50 - 5 * 6) / 4
+8 / 5   # division returns a floating point number
+8 // 5  # floor division discards the fractional part
+4 % 8   # modulus returns the remainder
+2 ** 2  # two squared
